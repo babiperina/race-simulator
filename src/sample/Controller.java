dd07456064a30809c5f9a2d54643acde1bab8d70
babiperina <k5bb22yv>
2017-10-02 12:18:14 -0300

    aa

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -6,11 +6,13 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Ellipse;
 
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
@@ -19,6 +21,12 @@ public class Controller implements Initializable {
     Pane main;
     @FXML
     Ellipse ellipse;
+
+    @FXML
+    Circle primeiroColocado;
+
+    @FXML
+    Circle segundoColocado;
 
     @FXML
     Button desenhar;
@@ -30,6 +38,7 @@ public class Controller implements Initializable {
     private double centroY;
 
     private double theta = 0;
+    private int velocidade = 0;
 
     private ArrayList<Circle> carros = new ArrayList<>();
 
@@ -37,41 +46,6 @@ public class Controller implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
         Runtime runtime = Runtime.getRuntime();
         qtdeCarros = runtime.availableProcessors();
-
-//        coordMaxX.setText("(" + ellipse.getBoundsInParent().getMaxX() + "," + centroY + ")");
-//        coordMinX.setText("(" + ellipse.getBoundsInParent().getMinX() + "," + centroY + ")");
-//        coordMaxY.setText("(" + centroY + "," + ellipse.getBoundsInParent().getMaxY() + ")");
-//        coordMinY.setText("(" + centroY + "," + ellipse.getBoundsInParent().getMinY() + ")");
-
-//        theta = 0;  // angle that will be increased each loop
-//        h = 12      // x coordinate of circle center
-//        k = 10      // y coordinate of circle center
-//        step = 15;  // amount to add to theta each time (degrees)
-//        var x = h +       r*Math.cos(theta) ;
-//        var y = k - 0.5 * r*Math.sin(theta) ;
-
-
-//        new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                for (int i = 0; i < 2; i++) {
-//                    for (theta = 0; theta < 2 * Math.PI; theta += step) {
-//                        Platform.runLater(() -> {
-//                            main.getChildren().remove(nvo2);
-//                            nvo2.setCenterX(centroX + ellipse.getRadiusX() * Math.cos(theta));
-//                            nvo2.setCenterY(centroY - 0.8 * ellipse.getRadiusY() * Math.sin(theta));
-//                            main.getChildren().add(nvo2);
-//                        });
-//                        try {
-//                            Thread.sleep(100);
-//                        } catch (InterruptedException e) {
-//                            e.printStackTrace();
-//                        }
-//                    }
-//                }
-//            }
-//        }).start();
-
     }
 
     public void printEllipse() {
@@ -84,15 +58,14 @@ public class Controller implements Initializable {
         centroX = (ellipse.getBoundsInParent().getMaxX() - ellipse.getBoundsInParent().getMinX()) / 2 + ellipse.getBoundsInParent().getMinX();
         centroY = (ellipse.getBoundsInParent().getMaxY() - ellipse.getBoundsInParent().getMinY()) / 2 + ellipse.getBoundsInParent().getMinY();
 
-        int distancia = 60;
+        int distancia = 50;
 
         for (int i = 0; i < qtdeCarros; i++) {
-            carros.add(new Circle(centroX + ellipse.getRadiusX() * Math.cos(theta - (02 * Math.PI / distancia)), centroY - ellipse.getRadiusY() * Math.sin(theta - (02 * Math.PI / distancia)), 5));
-            distancia -= 10;
+            carros.add(new Circle(centroX + ellipse.getRadiusX() * Math.cos(theta - (02 * Math.PI / distancia)), centroY - ellipse.getRadiusY() * Math.sin(theta - (02 * Math.PI / distancia)), 8, Paint.valueOf(getRandomColor())));
         }
 
         for (Circle carro : carros) {
-            System.out.println(carro);
+//            System.out.println(carro);
             main.getChildren().add(carro);
         }
 
@@ -103,27 +76,72 @@ public class Controller implements Initializable {
 
     public void comecarCorrida() {
         double step = 02 * Math.PI / 50;
+        theta = 0;
         new Thread(new Runnable() {
             @Override
             public void run() {
                 for (int i = 0; i < qtdeCarros; i++) {
                     for (theta = 0; theta < 2 * Math.PI; theta += step) {
                         Platform.runLater(() -> {
-                            main.getChildren().remove(carros);
                             for (Circle carro : carros) {
-                                carro.setCenterX(centroX + ellipse.getRadiusX() * Math.cos(theta));
-                                carro.setCenterY(centroY - ellipse.getRadiusY() * Math.sin(theta));
+                                main.getChildren().remove(carro);
+                                do {
+                                    velocidade = new Random().nextInt(20);
+                                } while (velocidade < 10);
+                                carro.setCenterX(centroX + ellipse.getRadiusX() * Math.cos(theta + 02 * Math.PI / velocidade));
+                                carro.setCenterY(centroY - ellipse.getRadiusY() * Math.sin(theta + 02 * Math.PI / velocidade));
+                                main.getChildren().add(carro);
                             }
-                            main.getChildren().addAll(carros);
                         });
                         try {
-                            Thread.sleep(100);
+                            Thread.sleep(50);
                         } catch (InterruptedException e) {
                             e.printStackTrace();
                         }
                     }
                 }
+                for (Circle carro : carros) {
+                    System.out.println(carro.toString());
+                }
+                verDoisPrimeirosColocados();
             }
         }).start();
+
+
+    }
+
+    public String getRandomColor() {
+        final Random random = new Random();
+        final String[] letters = "0123456789ABCDEF".split("");
+        String color = "#";
+        for (int i = 0; i < 6; i++) {
+            color += letters[Math.round(random.nextFloat() * 15)];
+        }
+        return color;
+    }
+
+    public void verDoisPrimeirosColocados() {
+        double menor = 0;
+        int imenor = 0;
+        for (int i = 0; i < carros.size(); i++) {
+            if (carros.get(i).getCenterY() < menor) {
+                menor = carros.get(i).getCenterY();
+                imenor = i;
+            }
+        }
+        primeiroColocado.setFill(carros.get(imenor).getFill());
+        primeiroColocado.setVisible(true);
+
+        double segundomenor = 0;
+        int isegundomenor = 0;
+        for (int i = 0; i < carros.size(); i++) {
+            if (carros.get(i).getCenterY() > segundomenor && isegundomenor != imenor) {
+                segundomenor = carros.get(i).getCenterY();
+                isegundomenor = i;
+            }
+        }
+
+        segundoColocado.setFill(carros.get(isegundomenor).getFill());
+        segundoColocado.setVisible(true);
     }
 }
