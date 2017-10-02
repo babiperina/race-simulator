2d58d18ddda29ef728bafce66167d4d37bbfb425
Bárbara Perina <babiperina@edu.unifor.br>
2017-10-02 05:56:21 -0300

    quase pronto

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,12 +3,14 @@ package sample;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Ellipse;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
@@ -18,10 +20,24 @@ public class Controller implements Initializable {
     @FXML
     Ellipse ellipse;
 
-    double theta = 0;
+    @FXML
+    Button desenhar;
+    @FXML
+    Button start;
+
+    private int qtdeCarros;
+    private double centroX;
+    private double centroY;
+
+    private double theta = 0;
+
+    private ArrayList<Circle> carros = new ArrayList<>();
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        Runtime runtime = Runtime.getRuntime();
+        qtdeCarros = runtime.availableProcessors();
+
 //        coordMaxX.setText("(" + ellipse.getBoundsInParent().getMaxX() + "," + centroY + ")");
 //        coordMinX.setText("(" + ellipse.getBoundsInParent().getMinX() + "," + centroY + ")");
 //        coordMaxY.setText("(" + centroY + "," + ellipse.getBoundsInParent().getMaxY() + ")");
@@ -64,42 +80,50 @@ public class Controller implements Initializable {
         System.out.println("D: " + ellipse.getBoundsInLocal().getWidth());
     }
 
-    public void iniciarCorrida() {
-        double centroX = (ellipse.getBoundsInParent().getMaxX() - ellipse.getBoundsInParent().getMinX()) / 2 + ellipse.getBoundsInParent().getMinX();
-        double centroY = (ellipse.getBoundsInParent().getMaxY() - ellipse.getBoundsInParent().getMinY()) / 2 + ellipse.getBoundsInParent().getMinY();
+    public void desenharCarros() {
+        centroX = (ellipse.getBoundsInParent().getMaxX() - ellipse.getBoundsInParent().getMinX()) / 2 + ellipse.getBoundsInParent().getMinX();
+        centroY = (ellipse.getBoundsInParent().getMaxY() - ellipse.getBoundsInParent().getMinY()) / 2 + ellipse.getBoundsInParent().getMinY();
 
-        Circle nvo = new Circle();
-        nvo.setRadius(5);
-        Circle nvo2 = new Circle();
-        nvo2.setRadius(5);
-        nvo2.setFill(Color.RED);
-        nvo.setCenterX(centroX + ellipse.getRadiusX() * Math.cos(theta- (02 * Math.PI / 20)));
-        nvo.setCenterY(centroY - ellipse.getRadiusY() * Math.sin(theta - (02 * Math.PI / 20)));
-        nvo2.setCenterX(centroX + ellipse.getRadiusX() * Math.cos(theta - (02 * Math.PI / 40)));
-        nvo2.setCenterY(centroY - ellipse.getRadiusY() * Math.sin(theta - (02 * Math.PI / 40)));
-        main.getChildren().add(nvo);
-        main.getChildren().add(nvo2);
+        int distancia = 60;
 
-//        double step = 02 * Math.PI / 50;
-//        new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                for (int i = 0; i < 3; i++) {
-//                    for (theta = 0; theta < 2 * Math.PI; theta += step) {
-//                        Platform.runLater(() -> {
-//                            main.getChildren().remove(nvo);
-//                            nvo.setCenterX(centroX + ellipse.getRadiusX() * Math.cos(theta));
-//                            nvo.setCenterY(centroY - ellipse.getRadiusY() * Math.sin(theta));
-//                            main.getChildren().add(nvo);
-//                        });
-//                        try {
-//                            Thread.sleep(100);
-//                        } catch (InterruptedException e) {
-//                            e.printStackTrace();
-//                        }
-//                    }
-//                }
-//            }
-//        }).start();
+        for (int i = 0; i < qtdeCarros; i++) {
+            carros.add(new Circle(centroX + ellipse.getRadiusX() * Math.cos(theta - (02 * Math.PI / distancia)), centroY - ellipse.getRadiusY() * Math.sin(theta - (02 * Math.PI / distancia)), 5));
+            distancia -= 10;
+        }
+
+        for (Circle carro : carros) {
+            System.out.println(carro);
+            main.getChildren().add(carro);
+        }
+
+        desenhar.setDisable(true);
+
+    }
+
+
+    public void comecarCorrida() {
+        double step = 02 * Math.PI / 50;
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < qtdeCarros; i++) {
+                    for (theta = 0; theta < 2 * Math.PI; theta += step) {
+                        Platform.runLater(() -> {
+                            main.getChildren().remove(carros);
+                            for (Circle carro : carros) {
+                                carro.setCenterX(centroX + ellipse.getRadiusX() * Math.cos(theta));
+                                carro.setCenterY(centroY - ellipse.getRadiusY() * Math.sin(theta));
+                            }
+                            main.getChildren().addAll(carros);
+                        });
+                        try {
+                            Thread.sleep(100);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }).start();
     }
 }
